f45f4f39d3d95c235fd689eb830e6a17f36be4f4
pickovven <opickford@gmail.com>
2011-04-21 20:45:48 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlatEarthMuseum::Application.config.secret_token = 'e2693bbbdce380338aecae841718e08e5d105e1dbf644d4b94df49ad632145f0e9200e48326e85591164749b4e39e216514dce99df9a84ebe371919fb5bc8ded'
