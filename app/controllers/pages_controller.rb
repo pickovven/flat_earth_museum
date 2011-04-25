02bac60e01034c91c5c8bfad0a1bfd3fd238453d
pickovven <opickford@gmail.com>
2011-04-25 17:07:25 -0400

    Dynamic Page Names

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,26 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def visit
+    @title = "Visit"
   end
 
   def about
+    @title = "About"
   end
 
   def members
+    @title = "Members"
   end
 
   def store
+    @title = "Store"
   end
 
   def contact
+    @title = "Contact"
   end
 
 end
