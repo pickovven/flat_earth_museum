096c2fdf782e25374f767c5468274e94d974e3c8
pickovven <opickford@gmail.com>
2011-05-09 18:46:01 -0400

    Different Background & JQuery written for Rails

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,14 @@
 module PagesHelper
 
   def slideshow()
-	function = "$('#slideshow1').cycle()"
+	function = 
+		"$(document).ready(function() {
+			$('#slideshow1').cycle({
+			fx: 'fade',	   // transition type
+			timeout: 5000  // time (ms) between beginning of each transition
+			});
+		});"
+	out = javascript_tag(function)
+	return out
   end
 end
