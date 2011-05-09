b3286b3611c5ac927518ab1a948d024306364564
pickovven <opickford@gmail.com>
2011-05-09 15:59:42 -0400

    slideshow partially done

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,10 +1,6 @@
 module PagesHelper
 
   def slideshow()
-	function = "$(document).ready(function(){
-				('a').click(function(event){
-				alert('Thanks for visiting!');
-				});
-			});"
+	function = "$('#slideshow1').cycle()"
   end
 end
